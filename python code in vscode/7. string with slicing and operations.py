631e02deec7d834207818aa224d6d9618187833c
amit <amitbudhwar0@gmail.com>
2023-01-06 15:12:55 +0530

    5-6jan. work

---
+++ python code in vscode/7. string with slicing and operations.py
@@ -0,0 +1,16 @@
+# this is slicing
+# this is slicing
+# this is slicing
+
+name="Amit,budhwa"
+print(name[0:5])
+print(name[1:5])
+print(len(name))
+
+
+# nagative slicing
+# nagative slicing
+# nagative slicing
+# neagative use for back side count
+
+print(name[1:-3])
